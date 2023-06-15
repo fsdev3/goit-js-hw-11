586c32d84490c6dc4f054c9e0a0c681ae44bf048
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-06-15 19:32:15 +0200

    3 funct

--- src/index.js
+++ src/index.js
@@ -1,3 +1,73 @@
+// import { onSubmit } from './pic-api.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+const form = document.querySelector('.search-form');
+const searchQuery = document.querySelector('input[name="searchQuery"]');
+form.addEventListener('submit', onSubmit);
+const axios = require('axios').default;
+const galleryItems = document.querySelector('.gallery');
 
-https://pixabay.com/api/
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '37263495-0dc17f57687021d8824007ffe';
+
+function onSubmit(event) {
+  event.preventDefault();
+  const inputValue = searchQuery.value;
+  searchImg(inputValue);
+}
+
+function searchImg(inputValue) {
+  return axios
+    .get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: inputValue,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+      },
+    })
+    .then(response => {
+      const objectsResponse = response.data.hits;
+      createGalleryItems(objectsResponse);
+      console.log(objectsResponse);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .finally(function () {});
+}
+
+function createGalleryItems(objectsResponse) {
+  const galleryHTML = objectsResponse
+    .map(
+      item => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.largeImageURL}">
+  <img class="gallery__image" src="${item.previewURL}" alt="${item.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${item.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${item.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${item.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${item.downloads}</b>
+    </p>
+  </div>
+  </div></a>
+`
+    )
+    .join('');
+
+  galleryItems.insertAdjacentHTML('beforeend', galleryHTML);
+
+  let lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: '250',
+    captionsData: 'alt',
+  });
+}
---
+++ src/pic-api.js
@@ -0,0 +1,17 @@
+// import axios from 'axios';
+
+// function getCat(inputValue) {
+//   fetch(BASE_URL + inputValue + API_KEY + '&q=')
+//     .then(r => {
+//       if (r.ok) {
+//         // return r.json();
+//         console.log(inputValue);
+//       }
+//       throw new Error(r.statusText || r.status);
+//     })
+//     .catch(error => {
+//       console.log('Oops! Something went wrong!');
+//       console.log(error);
+//     })
+//     .finally(() => {});
+// }
